59b7659b1ab7cdcb8a732abbc4d77c37cde2ccc7
Jaap Jansma <jaap@edeveloper.nl>
2015-12-15 12:08:13 +0100

    issue #114 rapport voor facturatie

---
+++ CRM/Casereports/Form/Report/Facturatie.mgd.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Jaap Jansma (CiviCooP) <jaap.jansma@civicoop.org>
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ */
+return array (
+    0 =>
+        array (
+            'name' => 'CRM_Casereports_Form_Report_Facturatie',
+            'entity' => 'ReportTemplate',
+            'params' =>
+                array (
+                    'version' => 3,
+                    'label' => 'Facturatie rapport',
+                    'description' => 'Een overzicht van coachingstrajecten die afgerond zijn en gefactureerd kunnen worden.',
+                    'class_name' => 'CRM_Casereports_Form_Report_Facturatie',
+                    'report_url' => 'be.werkenmetzin.casereports/facturatie',
+                    'component' => 'CiviCase',
+                ),
+        ),
+);
